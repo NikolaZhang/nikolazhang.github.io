cff5ce4e32cda875a4261cef32319518fdebbf49
NikolaZhang <NikolaZhang@users.noreply.github.com>
2025-12-01 11:57:48 +0000

    Deploying to gh-pages from @ NikolaZhang/nikolazhang.github.io@7b8569e22347f13f6c18f592a96d5a43e9591870 🚀

--- assets/pageview-B-axXwnN.js
+++
@@ -1,7 +0,0 @@
-import{_ as o}from"./app-CYrfdzXE.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://vercel-blog-comment.nikolazh.eu.org/",meta:["nick","mail","link"],requiredMeta:["nick"],reaction:!0,emoji:["//unpkg.com/@waline/emojis@1.1.0/bilibili","//unpkg.com/@waline/emojis@1.1.0/alus","//unpkg.com/@waline/emojis@1.1.0/qq","//unpkg.com/@waline/emojis@1.1.0/tieba","//unpkg.com/@waline/emojis@1.1.0/tw-emoji","//unpkg.com/@waline/emojis@1.1.0/weibo"]};const r=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-CYrfdzXE.js").then(i=>i.W),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{r as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
